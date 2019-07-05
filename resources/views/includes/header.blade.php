e34dd7fa08c39c7297eafa601ba549ff6257d801
Parag <16arag04utta@gmail.com>
2019-07-05 22:42:54 +0530

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,7 +8,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{{ URL::to('dashboard') }}">Alumni Association - GCELT</a>
+				<a class="navbar-brand" href="{{ URL::to('dashboard') }}">Social Network</a>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				{{-- <ul class="nav navbar-nav">
